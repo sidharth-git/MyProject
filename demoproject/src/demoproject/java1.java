fc1c0f8745d7c68888372243d63a8339585676ca
sidharth <sidharthmishra.ac@gmail.com>
2022-09-29 06:17:58 +0530

    File Updated.

---
+++ demoproject/src/demoproject/java1.java
@@ -0,0 +1,11 @@
+package demoproject;
+
+public class java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
---
+++ demoproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sidharth
+ *
+ */
+module demoproject {
+}
