260f28b98e96629c75d9fa1e461c87860fd7aa69
sidharth <sidharthmishra.ac@gmail.com>
2022-09-29 06:36:11 +0530

    Print status changed from hello world to hello sidharth.

--- demoproject/src/demoproject/java1.java
+++ demoproject/src/demoproject/java1.java
@@ -4,7 +4,7 @@ public class java1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO SIDHARTH");
 
 	}
 
